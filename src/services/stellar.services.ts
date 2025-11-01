827f817c652b01ca95fb0a739249a24aade7526c
mauro.ojeda.f <mauro.ojeda.f@gmail.com>
2025-10-31 23:20:11 -0300

    Cambios Clase 2

--- src/services/stellar.services.ts
+++ src/services/stellar.services.ts
@@ -1,27 +1,82 @@
-import { Horizon, Keypair } from "@stellar/stellar-sdk";
+import { Horizon, Keypair, TransactionBuilder, BASE_FEE, Asset, Operation } from "@stellar/stellar-sdk";
 import {
   STELLAR_NETWORK,
   HORIZON_URL,
   STELLAR_FRIENDBOT_URL,
+  HORIZON_NETWORK_PASSPHRASE,
 } from "../utils/contants";
 import { IKeypair } from "../interfaces/keypair";
-import { IAccountBalanceResponse } from "../interfaces/balance";
 import { AccountBalance } from "../interfaces/account";
+import { IAccountBalanceResponse } from "../interfaces/balance";
 
 export class StellarService {
   private network: string;
   private horizonUrl: string;
   private server: Horizon.Server;
   private friendBotUrl: string;
+  private networkPassphrase: string;
 
   constructor() {
     this.network = STELLAR_NETWORK as string;
     this.horizonUrl = HORIZON_URL as string;
     this.friendBotUrl = STELLAR_FRIENDBOT_URL as string;
+    this.networkPassphrase = HORIZON_NETWORK_PASSPHRASE as string;
 
     this.server = new Horizon.Server(this.horizonUrl, {
       allowHttp: true,
     });
+  }
+
+  private async loadAccount(address: string): Promise<Horizon.AccountResponse> {
+    try {
+      return await this.server.loadAccount(address);
+    } catch (error) {
+      console.error(error);
+      throw new Error("Account not found");
+    }
+  }
+
+  async payment(
+    senderPubKey: string,
+    senderSecret: string,
+    receiverPubKey: string,
+    amount: string
+  ): Promise<Horizon.HorizonApi.SubmitTransactionResponse> {
+    const sourceAccount = await this.loadAccount(senderPubKey);
+    const sourceKeypair = Keypair.fromSecret(senderSecret);
+
+    const transaction = new TransactionBuilder(sourceAccount, {
+      networkPassphrase: this.networkPassphrase,
+      fee: BASE_FEE,
+      //fee: 100,
+    })
+      .addOperation(
+        Operation.payment({
+          amount,
+          asset: Asset.native(),
+          destination: receiverPubKey,
+        })
+      )
+      .setTimeout(180)
+      .build();
+
+    transaction.sign(sourceKeypair);
+
+    try {
+      const result = await this.server.submitTransaction(transaction);
+
+      return result;
+    } catch (error) {
+      console.error(error);
+      if (error.response?.data?.extras?.result_codes) {
+        console.error(
+          "❌ Error en la transacción:",
+          error.response.data.extras.result_codes
+        );
+      } else {
+        console.error("❌ Error general:", error);
+      }
+    }
   }
 
   private async getAccount(address: string): Promise<Horizon.AccountResponse> {
